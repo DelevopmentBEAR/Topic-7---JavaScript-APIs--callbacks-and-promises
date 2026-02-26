d5714d72edc38cebee2c98644dca0fae4ad21fb9
Berit Haugen <movies.tv.and.music@gmail.com>
2026-02-26 13:54:54 -0600

    APOD API - data processing and display

--- apod/fetch_apod.js
+++ apod/fetch_apod.js
@@ -3,18 +3,54 @@ let apodExplanation = document.querySelector('#apod-explanation')
 let apodImage = document.querySelector('#apod-image')
 let apodVideo = document.querySelector('#apod-video')
 
-let apodApiUrl = 'https://api.nasa.gov/planetary/apod?api_key=ohdCtR3WBJuAXwlaRdsOgj7bLeI3euZXepSLvMgS'
+let apodApiUrl = 'https://api.nasa.gov/planetary/apod?api_key=4mUBxlioTv7duJ8ef3MqlrtJebk0s3tvatb5MMaG'
 
 // Fetch can fetch many kinds of web resources (e.g. images, other web pages), not just JSON.
 // It has to convert the response into JSON.
 // But, it could be quite slow for a large JSON response.
 fetch(apodApiUrl) // Fetch returns a Promise. The promise will Resolve = be successful (then) or be rejected = fail/error (catch)
-    .then(response => { // Response is the raw response data - it's bytes that can be found turn into JSON (hopefully)
+    .then( response => { // Response is the raw response data - it's bytes that can be found turn into JSON (hopefully)
         return response.json // Extract JSON from the response
     }) 
-    .then( data =>
+    // Below is where we get our specific data to return to html
+    .then( data => {
         console.log(data)
-    ) // The then function is called if the Promise is succesful
+
+        // Grabs title from apod, call it 'title'
+        let title = data.title
+        // Return 'title' to html
+        apodTitle.innerHTML = title
+
+        // Grabs explanation, we give it a name, return to html
+        let explanation = data.explanation
+        apodExplanation.innerHTML = explanation
+
+        // Asks data for media type
+        let mediaType = data.media_type
+
+        // If media type = image,
+        if (mediaType === 'image'){
+            // Connect to the image url and send back to html
+            apodImage.src = url
+            // Unhide image on html
+            apodImage.style.display = 'block'
+        }
+        // Else if media type = video
+        else if (mediaType === 'video'){
+            // Connect to the video url and send back to html
+            apodVideo.src = url
+            // Unhide video on html 
+            apodVideo.style.display = 'block'
+        }
+        else {
+            // Otherwise, send an error message
+            alert('Unknown media type')
+        }
+
+        // Same as above but with video
+        let video = data.video
+        apodVideo.innerHTML = video
+    }) // The then function is called if the Promise is succesful
     .catch( error => {
         console.log(error)
         apodExplanation.innerHTML = 'Sorry, could not fetch the astronomy picture.'
